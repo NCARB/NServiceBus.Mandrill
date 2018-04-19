20936994bdfb8bce51c6d752f4293ca69acdb1e5
Dejan Susic <dejo.susic@gmail.com>
2018-04-19 16:32:32 -0400

    Refactor Main code to work with nsb6

--- src/NServiceBus.Mandrill/ConfigureMandrill.cs
+++ src/NServiceBus.Mandrill/ConfigureMandrill.cs
@@ -1,31 +1,34 @@
 using System;
 using Mandrill;
 using NServiceBus.Configuration.AdvanceExtensibility;
+using NServiceBus.Mandrill;
 
 namespace NServiceBus
 {
     public static class ConfigureMandrill
     {
-        public static BusConfiguration UseMandrill(this BusConfiguration settings, string apiKey, bool replyResult = false)
+        public static EndpointConfiguration UseMandrill(this EndpointConfiguration settings, string apiKey, bool replyResult = false)
         {
-            if (apiKey == null) throw new ArgumentNullException("apiKey");
+            if (apiKey == null) throw new ArgumentNullException(nameof(apiKey));
             return UseMandrill(settings, new MandrillApi(apiKey), replyResult);
         }
 
-        public static BusConfiguration UseMandrill(this BusConfiguration settings, MandrillApi api, bool replyResult = false)
+        public static EndpointConfiguration UseMandrill(this EndpointConfiguration settings, MandrillApi api, bool replyResult = false)
         {
-            if (api == null) throw new ArgumentNullException("api");
+            if (api == null) throw new ArgumentNullException(nameof(api));
 
             return UseMandrill(settings, api.Messages, replyResult);
         }
 
-        private static BusConfiguration UseMandrill(this BusConfiguration settings, IMandrillMessagesApi mandrillApi, bool replyResult = false)
+        private static EndpointConfiguration UseMandrill(this EndpointConfiguration settings, IMandrillMessagesApi mandrillApi, bool replyResult = false)
         {
             settings.GetSettings().Set("NServiceBus.Mandrill.ReplyResult", replyResult);
             
             settings.RegisterComponents(x => x.RegisterSingleton(mandrillApi));
 
-            settings.EnableFeature<Features.Mandrill>();
+            var conventions = settings.Conventions().Conventions;
+            conventions.AddSystemMessagesConventions(t => typeof(MandrillEmailResult).IsAssignableFrom(t));
+            conventions.AddSystemMessagesConventions(t => typeof(SendMandrillEmail).IsAssignableFrom(t));
             return settings;
         }
     }
